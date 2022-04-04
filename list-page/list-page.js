de0de3aa1a10cd3e1c4f8cc85b2f99f29e8561f9
Beau Elliott <belliott1516@gmail.com>
2022-04-04 16:43:10 -0700

    styling and created rendering for list page

--- list-page/list-page.js
+++ list-page/list-page.js
@@ -1,5 +1,7 @@
 import { checkAuth, logout, getProfiles } from '../fetch-utils.js';
 
+const profileContainerEl = document.getElementById('profiles-container');
+
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
@@ -9,5 +11,28 @@ logoutButton.addEventListener('click', () => {
 });
 
 
-const profile = await getProfiles()
-console.log(profile);
+window.addEventListener('load', async () =>{
+    fetchAndDisplayProfiles();
+});
+
+async function fetchAndDisplayProfiles(){
+    profileContainerEl.textContent = '';
+
+    const profiles = await getProfiles();
+    console.log(profiles);
+
+    for (let profile of profiles){
+        const profileDiv = document.createElement('div');
+        const profileName = document.createElement('h3');
+        const karmaEl = document.createElement('p');
+
+        profileName.textContent = profile.email;
+        karmaEl.textContent = `Karma: ` + profile.karma ;
+
+        profileDiv.append(profileName, karmaEl);
+        profileDiv.classList.add('profile');
+        profileContainerEl.append(profileDiv);
+    }
+    
+}
+
